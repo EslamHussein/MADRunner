2e470db61f784907797ae8bb7e80eb9c10d05a83
GitHub Action <action@github.com>
2022-06-19 08:57:53 +0000

    Template cleanup

--- src/main/kotlin/com/github/eslamhussein/madrunner/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/eslamhussein/madrunner/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.eslamhussein.madrunner.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.eslamhussein.madrunner.MyBundle
 
 class MyApplicationService {
 
